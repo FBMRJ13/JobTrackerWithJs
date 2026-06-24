e9968d0a703ce2fa051efeb9410be3dedc892af0
FBMRJ13 <mstmita5113@gmail.com>
2026-06-24 11:01:48 +0600

    functionality of delete button

---
+++ script/delete.js
@@ -0,0 +1,52 @@
+function deleteBtn(card,status){
+
+const forAll=document.getElementById("totalCount");
+const forInt=document.getElementById("interviewCount");
+const forRej=document.getElementById("rejectedCount");
+
+
+ 
+ forAll.innerText=Number(forAll.innerText)-1;
+
+ const inText=(document.getElementById(status)).innerText;
+ if(inText=="Interview"){
+
+     forInt.innerText=Number(forInt.innerText)-1;
+     document.getElementById(card).remove();
+     const num=document.getElementById("interviewTab").children.length;
+     if(num==0){
+        const addChild=document.getElementById("noJob");
+            addChild.classList.remove("hidden");
+     }
+
+ }
+ else if(inText=="Rejected"){
+      forRej.innerText=Number(forRej.innerText)-1;
+      document.getElementById(card).remove();
+      const num=document.getElementById("rejectedTab").children.length;
+     if(num==0){
+        const addChild=document.getElementById("noJob");
+            addChild.classList.remove("hidden");
+     }
+
+ }
+
+ else if(inText=="Not Applied"){
+      
+      document.getElementById(card).remove();
+      const num=document.getElementById("allCardContainer").children.length;
+     if(num==0){
+        const addChild=document.getElementById("noJob");
+            addChild.classList.remove("hidden");
+     }
+
+ }
+
+ const updateNum=document.getElementById("updateNum");
+ updateNum.innerText=Number(updateNum.innerText)-1;
+ 
+
+
+
+
+}
